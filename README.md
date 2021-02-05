a83752c5d62e11f1541b28137af075b4af6d304d
VictorPankof <pankovych.v@gmail.com>
2021-02-05 13:50:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# m2portmone-preauth
+Magento 2 Portmone preauthorization payment method.
+This is module only for PWA storefront.
+Base m2 functionality comming soon.
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Registration module
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CodeCustom_PortmonePreAuthorization',
+    __DIR__
+);
